1fd94ba63751dc59d95c6a16934e8aa1802e0d6d
Sr. Mecha <sr.mecha@bk.ru>
2023-04-18 21:56:19 +0300

    fix

--- ScheduleSender/Program.cs
+++ ScheduleSender/Program.cs
@@ -6,5 +6,10 @@ namespace ScheduleSender;
 
 public static class Program
 {
-    public static void Main() => TelegramBot.Start().GetAwaiter().GetResult();
+    public static void Main() => MainAsync().GetAwaiter().GetResult();
+
+    public static async Task MainAsync()
+    {
+        await TelegramBot.Start();
+    }
 }
